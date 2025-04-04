70dcfc26a856c5bebc09693b82d1ae635e98a86e
SEAN ENG YI DA <seaneng2019@gmail.com>
2025-04-05 02:16:40 +0800

    shifted position of username lbl in customer login

--- Foodie Point Management System/Customer/CustomerLogin.Designer.cs
+++ Foodie Point Management System/Customer/CustomerLogin.Designer.cs
@@ -50,21 +50,19 @@
             // txtPassword
             // 
             this.txtPassword.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPassword.Location = new System.Drawing.Point(511, 314);
-            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPassword.Location = new System.Drawing.Point(341, 204);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(254, 35);
+            this.txtPassword.Size = new System.Drawing.Size(171, 26);
             this.txtPassword.TabIndex = 13;
             this.txtPassword.Visible = false;
             // 
             // txtInputs
             // 
             this.txtInputs.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtInputs.Location = new System.Drawing.Point(511, 250);
-            this.txtInputs.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtInputs.Location = new System.Drawing.Point(341, 162);
             this.txtInputs.Name = "txtInputs";
-            this.txtInputs.Size = new System.Drawing.Size(254, 35);
+            this.txtInputs.Size = new System.Drawing.Size(171, 26);
             this.txtInputs.TabIndex = 12;
             this.txtInputs.Visible = false;
             // 
@@ -73,10 +71,9 @@
             this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(212)))), ((int)(((byte)(194)))));
             this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLogin.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogin.Location = new System.Drawing.Point(530, 432);
-            this.btnLogin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnLogin.Location = new System.Drawing.Point(353, 281);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(158, 58);
+            this.btnLogin.Size = new System.Drawing.Size(105, 38);
             this.btnLogin.TabIndex = 11;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = false;
@@ -88,10 +85,9 @@
             this.lblPass.Enabled = false;
             this.lblPass.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPass.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.lblPass.Location = new System.Drawing.Point(398, 318);
-            this.lblPass.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPass.Location = new System.Drawing.Point(265, 207);
             this.lblPass.Name = "lblPass";
-            this.lblPass.Size = new System.Drawing.Size(106, 27);
+            this.lblPass.Size = new System.Drawing.Size(72, 18);
             this.lblPass.TabIndex = 10;
             this.lblPass.Text = "Password:";
             // 
@@ -100,10 +96,9 @@
             this.lblInputs.AutoSize = true;
             this.lblInputs.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblInputs.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.lblInputs.Location = new System.Drawing.Point(409, 253);
-            this.lblInputs.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblInputs.Location = new System.Drawing.Point(263, 164);
             this.lblInputs.Name = "lblInputs";
-            this.lblInputs.Size = new System.Drawing.Size(279, 27);
+            this.lblInputs.Size = new System.Drawing.Size(185, 18);
             this.lblInputs.TabIndex = 8;
             this.lblInputs.Text = "Please Pick a Login Method";
             // 
@@ -113,10 +108,9 @@
             this.cbSPassword.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.cbSPassword.Font = new System.Drawing.Font("Baskerville Old Face", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbSPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.cbSPassword.Location = new System.Drawing.Point(511, 376);
-            this.cbSPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cbSPassword.Location = new System.Drawing.Point(341, 244);
             this.cbSPassword.Name = "cbSPassword";
-            this.cbSPassword.Size = new System.Drawing.Size(139, 25);
+            this.cbSPassword.Size = new System.Drawing.Size(94, 18);
             this.cbSPassword.TabIndex = 17;
             this.cbSPassword.Text = "Show Password";
             this.cbSPassword.UseVisualStyleBackColor = true;
@@ -127,10 +121,9 @@
             this.rbUsername.AutoSize = true;
             this.rbUsername.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rbUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.rbUsername.Location = new System.Drawing.Point(458, 184);
-            this.rbUsername.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.rbUsername.Location = new System.Drawing.Point(305, 120);
             this.rbUsername.Name = "rbUsername";
-            this.rbUsername.Size = new System.Drawing.Size(134, 31);
+            this.rbUsername.Size = new System.Drawing.Size(92, 22);
             this.rbUsername.TabIndex = 16;
             this.rbUsername.Text = "Username";
             this.rbUsername.UseVisualStyleBackColor = true;
@@ -141,10 +134,9 @@
             this.rbEmail.AutoSize = true;
             this.rbEmail.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rbEmail.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.rbEmail.Location = new System.Drawing.Point(677, 184);
-            this.rbEmail.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.rbEmail.Location = new System.Drawing.Point(451, 120);
             this.rbEmail.Name = "rbEmail";
-            this.rbEmail.Size = new System.Drawing.Size(93, 31);
+            this.rbEmail.Size = new System.Drawing.Size(64, 22);
             this.rbEmail.TabIndex = 15;
             this.rbEmail.Text = "Email";
             this.rbEmail.UseVisualStyleBackColor = true;
@@ -156,10 +148,9 @@
             this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnRegister.Font = new System.Drawing.Font("Baskerville Old Face", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRegister.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.btnRegister.Location = new System.Drawing.Point(62, 481);
-            this.btnRegister.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnRegister.Location = new System.Drawing.Point(41, 313);
             this.btnRegister.Name = "btnRegister";
-            this.btnRegister.Size = new System.Drawing.Size(228, 35);
+            this.btnRegister.Size = new System.Drawing.Size(152, 23);
             this.btnRegister.TabIndex = 16;
             this.btnRegister.Text = "Register Now";
             this.btnRegister.UseVisualStyleBackColor = false;
@@ -170,10 +161,9 @@
             this.btnEmployee.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(212)))), ((int)(((byte)(194)))));
             this.btnEmployee.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnEmployee.Font = new System.Drawing.Font("Baskerville Old Face", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEmployee.Location = new System.Drawing.Point(767, 501);
-            this.btnEmployee.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnEmployee.Location = new System.Drawing.Point(511, 326);
             this.btnEmployee.Name = "btnEmployee";
-            this.btnEmployee.Size = new System.Drawing.Size(118, 58);
+            this.btnEmployee.Size = new System.Drawing.Size(79, 38);
             this.btnEmployee.TabIndex = 17;
             this.btnEmployee.Text = "Employee Login";
             this.btnEmployee.UseVisualStyleBackColor = false;
@@ -183,9 +173,10 @@
             // 
             this.lblExit.AutoSize = true;
             this.lblExit.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblExit.Location = new System.Drawing.Point(865, 11);
+            this.lblExit.Location = new System.Drawing.Point(577, 7);
+            this.lblExit.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblExit.Name = "lblExit";
-            this.lblExit.Size = new System.Drawing.Size(23, 24);
+            this.lblExit.Size = new System.Drawing.Size(15, 16);
             this.lblExit.TabIndex = 25;
             this.lblExit.Text = "X";
             this.lblExit.Click += new System.EventHandler(this.lblExit_Click);
@@ -198,18 +189,19 @@
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.btnRegister);
             this.panel1.Location = new System.Drawing.Point(-1, -1);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(359, 568);
+            this.panel1.Size = new System.Drawing.Size(239, 369);
             this.panel1.TabIndex = 26;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Baskerville Old Face", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(79, 433);
+            this.label3.Location = new System.Drawing.Point(53, 281);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(204, 24);
+            this.label3.Size = new System.Drawing.Size(137, 16);
             this.label3.TabIndex = 24;
             this.label3.Text = "Don\'t have an account?";
             // 
@@ -218,10 +210,9 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Palatino Linotype", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.label4.Location = new System.Drawing.Point(53, 96);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(35, 62);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(278, 194);
+            this.label4.Size = new System.Drawing.Size(186, 129);
             this.label4.TabIndex = 23;
             this.label4.Text = "🍵";
             // 
@@ -230,10 +221,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Palatino Linotype", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.label2.Location = new System.Drawing.Point(75, 301);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(50, 196);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(227, 48);
+            this.label2.Size = new System.Drawing.Size(148, 32);
             this.label2.TabIndex = 21;
             this.label2.Text = "Foodie Point";
             // 
@@ -242,18 +232,19 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Baskerville Old Face", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.label1.Location = new System.Drawing.Point(397, 48);
+            this.label1.Location = new System.Drawing.Point(265, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(118, 32);
+            this.label1.Size = new System.Drawing.Size(81, 22);
             this.label1.TabIndex = 27;
             this.label1.Text = "SIGN IN";
             // 
             // CustomerLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(235)))), ((int)(((byte)(226)))));
-            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.ClientSize = new System.Drawing.Size(600, 365);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblExit);
             this.Controls.Add(this.cbSPassword);
@@ -268,7 +259,6 @@
             this.Controls.Add(this.panel1);
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "CustomerLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
